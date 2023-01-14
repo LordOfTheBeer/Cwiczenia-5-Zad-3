c30975a6ae7316d8f66069d751cd80a9dd4b237a
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2023-01-14 14:20:41 +0100

    Add files via upload

---
+++ Cwiczenia 5 Zad 3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Cwiczenia_5_Zad_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 0;
+            int k = 0;
+
+            int[] tab1 = { 1, -2, 3, -4, 6 };
+
+            int[] tab2 = new int[5];
+
+            while (j < tab1.Length)
+            {
+                if (tab1[j] < 0)
+                {
+                    tab2[k] = tab1[j];
+                    k++;
+                }
+                j++;
+            }
+            foreach (int el in tab2)
+                Console.Write(el + " ");
+
+
+        }
+    }
+}
+    
